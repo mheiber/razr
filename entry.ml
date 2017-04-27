09d685616ac6beec4d369c3fbaf200797a5187db
Max Heiber <max.heiber@gmail.com>
2017-04-27 09:16:03 -0400

    functorize: MakeStore Functor

--- entry.ml
+++ entry.ml
@@ -1,71 +1,56 @@
+(* library *)
+module type Store = sig
+    type state_t
+    type action_t
+    val get_state : unit -> state_t
+    val subscribe : (unit -> unit) -> unit
+    val dispatch : action_t -> unit
+    val reduce : state_t -> action_t -> state_t
+end
+
+module type Store_def = sig
+    type state_t
+    type action_t
+    val state : state_t
+    val reduce : state_t -> action_t -> state_t
+end
+
+module MakeStore(Def: Store_def) : (Store
+    with type action_t := Def.action_t
+    with type state_t := Def.state_t) = struct
+    let state_ref = ref Def.state
+    let subscribers = ref []
+    let get_state () = !state_ref
+    let reduce x = Def.reduce x
+    let subscribe subscriber = subscribers := subscriber::!subscribers
+    let changed () = List.iter (fun subscriber -> subscriber ()) !subscribers
+    let dispatch action = state_ref := reduce !state_ref action; changed ();;
+end
+
+(* Creating a store *)
+
 type todo = { id: int; text: string }
-
 type state = { max_id : int; todos : todo list; }
-
-type action = 
+type action =
     | Create of string
     | Delete of string
 
-let create state = function
-    | Create(text) -> let max_id = state.max_id + 1 in
-                      let todo = { text; id = max_id } in
-                      { todos = todo::state.todos; max_id }
-    | _ -> state;;
+module Store = MakeStore(struct
+    type state_t = state
+    type action_t = action
+    let state = { max_id = 1; todos = []; } 
+    let reduce state = function
+        | Create(text) -> let max_id = state.max_id + 1 in
+                        let todo = { text; id = max_id } in
+                        { todos = todo::state.todos; max_id }
+        | Delete(text) -> { state with todos = List.filter (fun todo -> todo.text <> text) state.todos }
+end)
 
-let delete state = function
-    | Delete(text) -> { state with todos = List.filter (fun todo -> todo.text <> text) state.todos }
-    | _ -> state;;
-                
-let reducers = [create; delete]
-
-let reduce state action = 
-                         List.fold_left (fun state reducer -> reducer state action)
-                         state
-                         reducers;;
+(* utils for logging *)
 
 let print_todos todos =
     List.iter (fun x -> print_string (" - " ^ x.text ^ "\n")) todos;
     print_newline ();;
-(*val get_state : unit -> t*)
-
-module type StoreBase = sig
-    type t
-    val subscribe : (unit -> unit) -> unit
-    val dispatch : action -> unit
-end
-
-module type Z = sig
-    val x : int
-end
-module Z = struct
-    let x = 2
-end
-
-
-let create_Store state = 
-    (module struct
-        type t
-        let state_ref = ref state
-        let subscribers = ref []
-        let get_state () = !state_ref
-        let changed () = List.iter (fun subscriber -> subscriber ()) !subscribers
-        let subscribe subscriber = subscribers := subscriber::!subscribers
-        let dispatch action = state_ref := reduce !state_ref action; changed ();;
-     end : StoreBase with type t = state) ;;
-
-(*
-module Store = struct
-    type t = state
-    let state_ref = ref { max_id = 1; todos = []; }
-    let subscribers = ref []
-    let get_state () : t = !state_ref
-    let changed () = List.iter (fun subscriber -> subscriber ()) !subscribers
-    let subscribe subscriber = subscribers := subscriber::!subscribers
-    let dispatch action = state_ref := reduce !state_ref action; changed ();;
-
-end*)
-
-module Store = (val create_Store { max_id = 1; todos = []; } : StoreBase);;
 
 let string_of_action = function
         | Create(text) -> "Create(" ^ text ^ ")"
@@ -78,7 +63,9 @@ let dispatch action =
     print_newline ();
     Store.dispatch action;;
 
-let print_state state = print_todos (Store.get_state ()).todos;;
+let print_state state = print_todos ((Store.get_state ()).todos)
+
+(* do stuff *)
 
 let () =
        Store.subscribe print_state;
